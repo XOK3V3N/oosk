6d7aace4d059e37fd8e561e78f33d92f86e6abfd
Kevin Romero <kevinromero@Kevins-MacBook-Air.local>
2023-05-01 17:29:28 -0400

    intial commit

---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    year: "Today",
+    title: "Adjunct Professor",
+    duration: "2 years",
+    details: "I teach students computer science/C++!",
+  },
+  {
+    year: "Today",
+    title: "Software Engineer",
+    duration: "2 years",
+    details: "I write backend java programs :o",
+  },
+  {
+    year: "2022",
+    title: "Adjunct Professor",
+    duration: "2 years",
+    details: "I taught students Python!",
+  },
+];
